f4d08ffc7e2656f6a7175943ddb7847d664f21fc
Dennis Ludwig <dennis.ludwig@aoe.com>
2023-05-23 23:39:51 +0200

    rotate external screen if connected and reset after closing steam

--- reset screen orientation.sh
+++ reset screen orientation.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-xrandr -o right
+# Resets orientation of external screen if connected, else steam deck screen
+function resetScreenOrientation {
+  echo "reset"
+  screens="$(xrandr | grep -Po '().+(?= connected)')"
+  countScreens="$(echo "$screens" | wc -l)"
+
+  if [ "$countScreens" = 1 ]; then
+    # Reset steam deck screen
+    screenToReset="$(echo "$screens" | grep -m1 "")"
+    rotation="right"
+  else
+    # Reset external screen
+    screenToReset="$(echo "$screens" | grep -m2 "" | tail -n1)"
+    rotation="normal"
+  fi
+
+  xrandr --output "$screenToReset" --rotate "$rotation"
+}
+
+resetScreenOrientation
+
+exit 0
